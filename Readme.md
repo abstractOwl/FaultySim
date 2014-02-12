560388768358baaa611aaa98fc6af6b8557dd6ed
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-02-11 17:30:29 -0800

    Finish implementing packet size and fixed bug with repeat mode

--- Readme.md
+++ Readme.md
@@ -1,18 +1,21 @@
 # FaultySim
-Simulates the effect of network packet loss on streaming audio
+Simulates the effect of network packet loss on streaming uncompressed audio
 
 ## Usage
 
-	java Simulator <inFile> <outFile> <percent> <packet_size>
+	java Simulator <inFile> <outFile> <percent> <packet_size> [<mode>]
 		
-* **inFile**: Input file. Should be a Sun `.au` audio file. At this point, does not parse header to ensure it is an `.au` file.
+* **<inFile>**: Input file. Should be a Sun `.au` audio file. At this point, does not parse header to ensure it is an `.au` file.
 
-* **outFile**: Output file
+* **<outFile>**: Output file
 
-* **percent**: Simulated success rate
+* **<packet_size>**: Simulated packet size
 
-* **packet_size**: Simulated packet size
+* **<percent>**: Simulated success rate
+
+* **<mode>**: Optional, can be "silent" or "repeat". The technique used to fill in lost segments
 
 
-## TODO
-* Implement packet size
+## Future Work
+
+Use Java AudioFileFormat API for file IO. See http://docs.oracle.com/javase/tutorial/sound/converters.html
--- Simulator.java
+++ Simulator.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -11,10 +12,11 @@ public class Simulator {
   private static final int HEADER_LENGTH = 40;
   
   private final Mode   mode;
+  private final int    packetSize;
   private final int    percent;
   private final Random random;
   
-  private byte lastByte;
+  private byte[] last;
   
   public enum Mode { SILENT, REPEAT };
   
@@ -23,35 +25,38 @@ public class Simulator {
    * @param mode Strategy used to replace lost packets 
    * @param percent Percentage of packets sent successfully
    */
-  public Simulator(Mode mode, int percent) {
-    this.mode     = mode;
-    this.percent  = percent;
+  public Simulator(Mode mode, int packetSize, int percent) {
+    this.mode       = mode;
+    this.packetSize = packetSize; 
+    this.percent    = percent;
 
-    lastByte = 0;
+    last     = new byte[packetSize];
     random   = new Random();
   }
   
   /**
-   * Returns the input byte or a computed fake byte based on the specified
-   * probability.
-   * @param nextByte Byte to be computed on
-   * @return Resulting byte
+   * Returns the input byte array or a computed fake byte array based on the
+   * specified probability.
+   * @param buffer Byte array to be computed on
+   * @return Resulting byte array
    */
-  private byte applyLoss(byte nextByte) {
+  private byte[] applyLoss(byte[] buffer) {
     int randomNumber = random.nextInt(100) + 1; // Always loss if % = 0
     
     if (randomNumber <= percent) {
-      if (mode == Mode.REPEAT) lastByte = nextByte;
-      return nextByte;
+      if (mode == Mode.REPEAT) System.arraycopy(buffer, 0, last, 0, buffer.length);
+      return buffer;
     }
     
     // Simulate packet loss/compute fake byte
-    byte falseByte;
+    byte[] falseByte = new byte[buffer.length];
     switch (mode) {
-    case REPEAT: falseByte = lastByte;
-    default:     falseByte = 0;
+    case REPEAT:
+      System.arraycopy(last, 0, falseByte, 0, last.length);
+      break;
+    default:
+      Arrays.fill(falseByte, (byte) 0);
     }
-    if (mode == Mode.REPEAT) lastByte = falseByte;
     return falseByte;
   }
   
@@ -63,13 +68,19 @@ public class Simulator {
    */
   public void processFile(FileInputStream in, FileOutputStream out) {
     int next;
-    int bytesRead = 0;
+    
     try {
-      while ((next = in.read()) != -1) {
-        byte nextByte = (byte) next;
-        byte faultyByte = (bytesRead < HEADER_LENGTH) ? nextByte : applyLoss(nextByte);
-        out.write(faultyByte);
+      // Read header
+      int bytesRead = 0;
+      while ((next = in.read()) != -1 && bytesRead < HEADER_LENGTH) {
+        out.write((byte) next);
         bytesRead++;
+      }
+
+      byte[] buffer = new byte[packetSize];
+      while (in.read(buffer) != -1) {
+        byte[] faultyByte = applyLoss(buffer);
+        out.write(faultyByte);
       }
     } catch (IOException e) {
       throw new RuntimeException("Error while reading file.", e);
@@ -80,23 +91,36 @@ public class Simulator {
    * Prints the usage information to System.err.
    */
   public static void usage() {
-    System.err.println("USAGE: java Simulator <infile> <outfile> <0-100>");
+    System.err.println("USAGE: java Simulator <infile> <outfile> <packet_size> <percent> [<mode>]");
+    System.err.println("Options:");
+    System.err.println("  infile:      Input *.au file");
+    System.err.println("  outfile:     Output *.au file");
+    System.err.println("  packet_size: Simulated packet size");
+    System.err.println("  percent:     Simulated success rate [0-100]");
+    System.err.println("  mode:        Optional (\"silent\" or \"repeat\"). Technique used to fill in lost segments.");
     System.exit(-1);
   }
   
   public static void main(String[] args) {
-    if (args.length != 3) {
+    if (args.length != 4 && args.length != 5) {
       usage();
     }
     
-    String inFile  = args[0];
-    String outFile = args[1];
-    int    percent = Integer.parseInt(args[2], 10);
+    String inFile     = args[0];
+    String outFile    = args[1];
+    int    packetSize = Integer.parseInt(args[2], 10);
+    int    percent    = Integer.parseInt(args[3], 10);
+    
+    Mode mode = (args.length == 5 && args[4].equalsIgnoreCase("repeat"))
+        ? Mode.REPEAT : Mode.SILENT;
     
     // Check arguments
     if (percent < 0 || percent > 100) {
       usage();
     }
+    
+    long start = System.currentTimeMillis();
+    System.out.print("Processing file... ");
 
     // Initialize file objects
     FileInputStream  input;
@@ -113,8 +137,12 @@ public class Simulator {
     }
     
     // Write files
-    Simulator simulator = new Simulator(Mode.SILENT, percent);
+    Simulator simulator = new Simulator(mode, packetSize, percent);
     simulator.processFile(input, output);
+    
+    long elapsed = System.currentTimeMillis() - start;
+    System.out.println("done.");
+    System.out.println("Task completed in " + elapsed + "ms.");
 
     // Finalize objects
     try {
